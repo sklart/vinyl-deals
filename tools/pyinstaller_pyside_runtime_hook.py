92cb6a37014626928e4f47a322fc790f077fd517
sklart <sklart@yandex.ru>
2026-09-10 14:07:37 +0300

    Expose Shiboken DLL directory to Qt

--- tools/pyinstaller_pyside_runtime_hook.py
+++ tools/pyinstaller_pyside_runtime_hook.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 
 if os.name == "nt" and hasattr(sys, "_MEIPASS"):
-    pyside_directory = Path(sys._MEIPASS) / "PySide6"
-    if pyside_directory.is_dir():
-        os.add_dll_directory(str(pyside_directory))
+    for name in ("PySide6", "shiboken6"):
+        directory = Path(sys._MEIPASS) / name
+        if directory.is_dir():
+            os.add_dll_directory(str(directory))
